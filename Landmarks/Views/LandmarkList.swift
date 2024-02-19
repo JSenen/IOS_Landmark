6668d7458b2715484a1886fba0a2ee5a587746b9
Juan Senen <juansenen@hotmail.com>
2024-02-19 20:50:38 +0100

    Model Data

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -10,10 +10,12 @@ import SwiftUI
 
 struct LandmarkList: View {
     
+    @Environment(ModelData.self) var modelData
     @State private var showFavoritesOnly = false
     
+
     var filteredLandmarks: [Landmark] {
-            landmarks.filter { landmark in
+        modelData.landmarks.filter { landmark in
                 (!showFavoritesOnly || landmark.isFavorite)
             }
         }
@@ -45,4 +47,5 @@ struct LandmarkList: View {
 
 #Preview {
     LandmarkList()
+        .environment(ModelData())
 }
