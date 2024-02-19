421716a2f09d293a7bdb35677c6ad27db4754afe
Juan Senen <juansenen@hotmail.com>
2024-02-19 20:37:11 +0100

    Add toggle button

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -9,20 +9,37 @@ import SwiftUI
 
 
 struct LandmarkList: View {
+    
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+            landmarks.filter { landmark in
+                (!showFavoritesOnly || landmark.isFavorite)
+            }
+        }
+    
     var body: some View {
         NavigationSplitView {
-            List(landmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List {
+                //Toogle button
+                Toggle(isOn: $showFavoritesOnly) {
+                        Text("Favorites only")
+                        }
+                //combine two or more different groups of dynamic views,
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                        } label: {
+                        LandmarkRow(landmark: landmark)
+                        }
+                    }
                 }
+            .animation(.default, value: filteredLandmarks)
+                        .navigationTitle("Landmarks")
+            } detail: {
+                        Text("Select a Landmark")
             }
-            .navigationTitle("Landmarks")
-        } detail: {
-            Text("Select a Landmark")
         }
-    }
 }
 
 
